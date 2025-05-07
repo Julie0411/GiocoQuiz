c05e5742e24235fda997af1dce10bba72455a6c4
Julie0411 <150553151+Julie0411@users.noreply.github.com>
2025-05-07 09:21:53 +0200

    Added true false question

---
+++ src/DomandaSingolaRispostaCorretta.java
@@ -0,0 +1,29 @@
+package org.julie.progetto.modulo320.QuizGame;
+
+import java.util.List;
+
+public class DomandaSingolaRispostaCorretta extends Domanda {
+
+    private String rispostaCorretta;
+
+    public DomandaSingolaRispostaCorretta(String domanda, List<String> risposte, String rispostaCorretta) {
+        super(domanda, risposte);
+        if (rispostaCorretta == null) throw new IllegalArgumentException("Ci deve essere una risposta corretta!");
+        this.rispostaCorretta = rispostaCorretta;
+    }
+
+    @Override
+    public int valuta() {
+        int punteggio = 0;
+        for (String r : getRisposteDate()) {
+            if (r.equals(this.rispostaCorretta)) {
+                punteggio++;
+            } else {
+                if (punteggio > 0) {
+                    punteggio--;
+                }
+            }
+        }
+        return punteggio;
+    }
+}
