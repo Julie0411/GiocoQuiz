6526d1912b412c43ea832587b0bf61f8d63b0f40
Julie0411 <150553151+Julie0411@users.noreply.github.com>
2025-05-13 11:31:49 +0200

    modified tests

--- src/test/java/org/julie/progetto/modulo320/TestDomandaRispostaMultipla.java
+++ src/test/java/org/julie/progetto/modulo320/TestDomandaRispostaMultipla.java
@@ -1,7 +1,6 @@
 package org.julie.progetto.modulo320;
 
 import org.julie.progetto.modulo320.quizGame.DomandaRispostaMultipla;
-import org.julie.progetto.modulo320.quizGame.DomandaVeroFalso;
 import org.testng.annotations.Test;
 
 import java.util.Set;
@@ -13,32 +12,37 @@ public class TestDomandaRispostaMultipla {
 
     @Test
     public void test_domanda_risposta_multipla_se_passo_una_domanda_nulla_deve_lanciare_errore() {
-        assertThrows(IllegalArgumentException.class, ()-> {new DomandaRispostaMultipla(null, Set.of("a", "b", "c", "d"), Set.of("c"));});
+        assertThrows(IllegalArgumentException.class, ()-> {new DomandaRispostaMultipla(null, Set.of("a", "b", "c", "d"), Set.of("c"), "Italiano");});
     }
 
     @Test
     public void test_domanda_risposta_multipla_se_passo_null_come_risposte_corrette_deve_lanciare_errore() {
-        assertThrows(IllegalArgumentException.class, ()-> {new DomandaRispostaMultipla("domanda di prova", null, Set.of("c"));});
+        assertThrows(IllegalArgumentException.class, ()-> {new DomandaRispostaMultipla("domanda di prova", null, Set.of("c"), "Storia");});
     }
 
     @Test
     public void test_domanda_risposta_multipla_se_passo_la_risposta_corretta_come_nulla_deve_lanciare_errore() {
-        assertThrows(IllegalArgumentException.class, ()-> {new DomandaRispostaMultipla("domanda di prova", Set.of("a", "b", "c", "d"), null);});
+        assertThrows(IllegalArgumentException.class, ()-> {new DomandaRispostaMultipla("domanda di prova", Set.of("a", "b", "c", "d"), null, " Geografia");});
     }
 
     @Test
     public void test_domanda_risposta_multipla_se_passo_la_risposta_corretta_come_lista_vuota_deve_lanciare_errore() {
-        assertThrows(IllegalArgumentException.class, ()-> {new DomandaRispostaMultipla("domanda di prova", Set.of("a", "b", "c", "d"), Set.of());});
+        assertThrows(IllegalArgumentException.class, ()-> {new DomandaRispostaMultipla("domanda di prova", Set.of("a", "b", "c", "d"), Set.of(), "Scienze");});
     }
 
     @Test
     public void test_domanda_risposta_multipla_se_passo_un_elemento_insistente_come_risposta_corretta_deve_lanciare_errore() {
-        assertThrows(IllegalArgumentException.class, ()-> {new DomandaRispostaMultipla("domanda di prova", Set.of("a", "b", "c", "d"), Set.of("e"));});
+        assertThrows(IllegalArgumentException.class, ()-> {new DomandaRispostaMultipla("domanda di prova", Set.of("a", "b", "c", "d"), Set.of("e"), "Musica");});
+    }
+
+    @Test
+    public void test_domanda_risposta_multipla_se_passo_una_categoria_nulla_deve_lanciare_errore() {
+        assertThrows(IllegalArgumentException.class, ()-> {new DomandaRispostaMultipla("domanda di prova", Set.of("a", "b", "c", "d"), Set.of("a"), null);});
     }
 
     @Test
     public void test_domanda_risposta_multipla_se_la_mia_risposta_e_sbagliata_il_punteggio_deve_essere_0() {
-        DomandaRispostaMultipla domanda = new DomandaRispostaMultipla("domanda di prova", Set.of("a", "b", "c", "d"), Set.of("c"));
+        DomandaRispostaMultipla domanda = new DomandaRispostaMultipla("domanda di prova", Set.of("a", "b", "c", "d"), Set.of("c"), "Latino");
         Set<String> risposteDate = Set.of("d");
         domanda.setRisposteDate(risposteDate);
         assertEquals(0.0f, domanda.valuta());
@@ -46,7 +50,7 @@ public class TestDomandaRispostaMultipla {
 
     @Test
     public void test_domanda_risposta_multipla_se_ho_messo_una_risposta_giusta_e_una_sbagliata_il_punteggio_deve_essere_0_66() {
-        DomandaRispostaMultipla domanda = new DomandaRispostaMultipla("domanda di prova", Set.of("a", "b", "c", "d"), Set.of("c"));
+        DomandaRispostaMultipla domanda = new DomandaRispostaMultipla("domanda di prova", Set.of("a", "b", "c", "d"), Set.of("c"), "Cucina");
         Set<String> risposteDate = Set.of("c", "d");
         domanda.setRisposteDate(risposteDate);
         assertEquals(0.66f, domanda.valuta(), 2);
@@ -54,7 +58,7 @@ public class TestDomandaRispostaMultipla {
 
     @Test
     public void test_domanda_risposta_multipla_se__ho_messo_una_risposta_giusta_e_due_sbagliate_il_punteggio_deve_essere_0_33() {
-        DomandaRispostaMultipla domanda = new DomandaRispostaMultipla("domanda di prova", Set.of("a", "b", "c", "d"), Set.of("c"));
+        DomandaRispostaMultipla domanda = new DomandaRispostaMultipla("domanda di prova", Set.of("a", "b", "c", "d"), Set.of("c"), "Francese");
         Set<String> risposteDate = Set.of("b", "c", "d");
         domanda.setRisposteDate(risposteDate);
         assertEquals(0.33f, domanda.valuta(), 2);
@@ -62,7 +66,7 @@ public class TestDomandaRispostaMultipla {
 
     @Test
     public void test_domanda_risposta_multipla_se_la_mia_risposta_e_giusta_il_punteggio_deve_essere_1() {
-        DomandaRispostaMultipla domanda = new DomandaRispostaMultipla("domanda di prova", Set.of("a", "b", "c", "d"), Set.of("c"));
+        DomandaRispostaMultipla domanda = new DomandaRispostaMultipla("domanda di prova", Set.of("a", "b", "c", "d"), Set.of("c"), "Tedesco");
         Set<String> risposteDate = Set.of("c");
         domanda.setRisposteDate(risposteDate);
         assertEquals(1.0f, domanda.valuta());
--- src/test/java/org/julie/progetto/modulo320/TestDomandaVeroFalso.java
+++ src/test/java/org/julie/progetto/modulo320/TestDomandaVeroFalso.java
@@ -1,9 +1,9 @@
 package org.julie.progetto.modulo320;
 
-import org.julie.progetto.modulo320.QuizGame.DomandaVeroFalso;
+import org.julie.progetto.modulo320.quizGame.DomandaVeroFalso;
 import org.testng.annotations.Test;
 
-import java.util.List;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -12,26 +12,31 @@ public class TestDomandaVeroFalso {
 
     @Test
     public void test_domanda_vero_falso_se_passo_una_domanda_nulla_deve_lanciare_errore() {
-        assertThrows(IllegalArgumentException.class, ()-> {new DomandaVeroFalso(null, "Vero");});
+        assertThrows(IllegalArgumentException.class, ()-> {new DomandaVeroFalso(null, "Vero", "Biologia");});
     }
 
     @Test
     public void test_domanda_vero_falso_se_passo_la_risposta_corretta_come_nulla_deve_lanciare_errore() {
-        assertThrows(IllegalArgumentException.class, ()-> {new DomandaVeroFalso("Il cielo è azzurro?", null);});
+        assertThrows(NullPointerException.class, ()-> {new DomandaVeroFalso("Il cielo è azzurro?", null, "Chimica");});
+    }
+
+    @Test
+    public void test_domanda_vero_falso_se_passo_un_argomento_nullo_deve_lanciare_errore() {
+        assertThrows(IllegalArgumentException.class, ()-> {new DomandaVeroFalso("Il cielo è azzurro?", "vero", null);});
     }
 
     @Test
     public void test_domanda_vero_falso_se_la_mia_risposta_e_giusta_il_punteggio_deve_essere_1() {
-        DomandaVeroFalso domanda = new DomandaVeroFalso("Il cielo è azzurro?", "Vero");
-        List<String> risposteDate = List.of("Vero");
+        DomandaVeroFalso domanda = new DomandaVeroFalso("Il cielo è azzurro?", "Vero", "Fisica");
+        Set<String> risposteDate = Set.of("Vero");
         domanda.setRisposteDate(risposteDate);
         assertEquals(1, domanda.valuta());
     }
 
     @Test
     public void test_domanda_vero_falso_se_la_mia_risposta_e_sbagliata_il_punteggio_deve_essere_0() {
-        DomandaVeroFalso domanda = new DomandaVeroFalso("Il cielo è azzurro?", "Vero");
-        List<String> risposteDate = List.of("Falso");
+        DomandaVeroFalso domanda = new DomandaVeroFalso("Il cielo è azzurro?", "Vero", "Matematica");
+        Set<String> risposteDate = Set.of("Falso");
         domanda.setRisposteDate(risposteDate);
         assertEquals(0, domanda.valuta());
     }
